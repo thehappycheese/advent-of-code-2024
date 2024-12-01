0223238893d9d2b2af2b1f970965a4ff83e2a00f
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-12-01 14:52:31 +0800

    day1 p1 golfed

--- 01/p1/one-liner.js
+++ 01/p1/one-liner.js
@@ -1,11 +1,8 @@
-(
-    ab=document.body.innerText
-        .trim()
-        .split("\n")
-        .map(item=>item.split("   ").map(parseFloat))
-        .reduce((acc,cur)=>[
-            [...acc[0], cur[0]],
-            [...acc[1], cur[1]]
-        ],[[],[]])
-		.map(item=>item.sort())
-)[0].map((item_a, index)=>Math.abs(item_a-ab[1][index])).reduce((a,b)=>a+b);
+document.body.innerText
+.trim()
+.split("\n")
+.map(item=>item.split("   ").map(parseFloat))
+.reduce((acc, curr) => curr.map((item, i) => (acc[i] || []).concat(item)), [])
+.map(item=>item.sort())
+.reduce((a,b)=>a.map((a,i)=>Math.abs(a-b[i])))
+.reduce((a,b)=>a+b)
