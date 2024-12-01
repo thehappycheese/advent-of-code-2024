4e39fbbeb5e47ae17a28b1afc1e5a5aaa04bc805
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-12-01 13:53:09 +0800

    reduced sorting

--- 01/p1/one-liner.js
+++ 01/p1/one-liner.js
@@ -3,5 +3,9 @@
         .trim()
         .split("\n")
         .map(item=>item.split("   ").map(parseFloat))
-        .reduce((acc,cur)=>({a:[...acc.a, cur[0]].sort(), b:[...acc.b, cur[1]].sort()}),{a:[],b:[]})
-).a.map((item_a, index)=>Math.abs(item_a-ab.b[index])).reduce((a,b)=>a+b);
+        .reduce((acc,cur)=>[
+            [...acc[0], cur[0]],
+            [...acc[1], cur[1]]
+        ],[[],[]])
+		.map(item=>item.sort())
+)[0].map((item_a, index)=>Math.abs(item_a-ab[1][index])).reduce((a,b)=>a+b);
