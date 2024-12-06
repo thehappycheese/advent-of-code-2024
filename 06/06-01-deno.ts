a4db43089a16f1d775836963fccfa57d3bbf41b9
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-12-06 22:27:27 +0800

    corrections

--- 06/06-01-deno.ts
+++ 06/06-01-deno.ts
@@ -3,15 +3,15 @@ const lines = input.trim().split("\n");
 const cells = lines.map(item=>item.split(""));
 type Position = [row:number, col:number];
 let guard:Position;
-const size_y =  lines.length;
-const size_x = lines[0].length;
+const size_r =  lines.length;
+const size_c = lines[0].length;
 cells.forEach((row,row_index)=>row.forEach((cell, col_index)=>cell==="^"?(guard=[row_index, col_index]):""));
-const to_index = ([x, y]:Position) => y*size_x+x;
+const to_index = ([r, c]:Position) => r*size_c+c;
 const add = (a:Position, b:Position):Position=>[a[0]+b[0],a[1]+b[1]];
 const right = (a:Position):Position => [a[1], -a[0]];
 let direction:Position = [-1,0];
 const visited:Map<number, boolean> = new Map();
-while (guard[0]>=0 && guard[0]<size_x && guard[1]>=0 && guard[1]<size_y){
+while (guard[0]>=0 && guard[0]<size_r && guard[1]>=0 && guard[1]<size_c){
     let next_position = add(guard, direction);
     if(cells[next_position[0]][next_position[1]]==="#"){
         direction = right(direction);
