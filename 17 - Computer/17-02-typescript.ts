0ad903d3a1d42cd6e9efe5a7d31dfffa149c0696
thehappycheese <2748120+thehappycheese@users.noreply.github.com>
2024-12-19 17:44:02 +0800

    nearly cracked

--- 17 - Computer/17-02-typescript.ts
+++ 17 - Computer/17-02-typescript.ts
@@ -1,129 +1,5 @@
+import { Computer, run, decompile, run_until_mismatch, print_state, run_to_console, run_to_list } from "./util/computer.ts";
 
-
-
-type Computer = {
-    A:number
-    B:number
-    C:number
-    program:number[]
-    pointer:number
-}
-
-type Instruction = (stdout:(out:number)=>void)=>(c:Computer) => (
-    ((cb:Combo) => Computer)| ((cb:Literal) => Computer)
-)
-
-type Combo = 0|1|2|3|4|5|6;
-type Literal = number;
-
-const combo = (c:Computer)=> (combo:Combo)=>{
-    switch(combo){
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-            return combo
-        case 4:
-            return c.A
-        case 5:
-            return c.B
-        case 6:
-            return c.C
-        default:
-            throw new Error("Invalid combo")
-    }
-}
-
-const adv:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(cb:Combo)=>({
-    ...cp,
-    pointer:cp.pointer+2,
-    A:Math.floor(
-        cp.A / Math.pow(2,combo(cp)(cb))
-    )
-});
-
-const bxl:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(lit:number)=>({
-    ...cp,
-    pointer:cp.pointer+2,
-    B:cp.B^lit
-});
-
-const bst:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(cb:Combo)=>({
-    ...cp,
-    pointer:cp.pointer+2,
-    B:combo(cp)(cb)&7
-});
-
-const jnz:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(lit:number)=>({
-    ...cp,
-    pointer:cp.A===0?cp.pointer+2:cp.pointer=lit,
-});
-
-const bxc:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(_:number)=>({
-    ...cp,
-    pointer:cp.pointer+2,
-    B:cp.B^cp.C
-});
-
-const out:Instruction = (stdout:(out:number)=>void)=>(cp:Computer)=>(cb:Combo)=>{
-    stdout(combo(cp)(cb)%8)
-    return {
-        ...cp,
-        pointer:cp.pointer+2,
-    }
-};
-
-const bdv:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(cb:Combo)=>({
-    ...cp,
-    pointer:cp.pointer+2,
-    B:Math.floor(
-        cp.A / Math.pow(2,combo(cp)(cb))
-    )
-});
-
-const cdv:Instruction = (_stdout:(out:number)=>void)=>(cp:Computer)=>(cb:Combo)=>({
-    ...cp,
-    pointer:cp.pointer+2,
-    C:Math.floor(
-        cp.A / Math.pow(2,combo(cp)(cb))
-    )
-});
-
-const decode = (opcode:number) => [
-    adv,bxl,bst,jnz,bxc,out,bdv,cdv
-][opcode];
-const describe_combo = (cb:Combo)=>({
-        0:"0",
-        1:"1",
-        2:"2",
-        3:"3",
-        4:"A",
-        5:"B",
-        6:"C",
-    }[cb])
-const describe = (offset:number) => (opcode:number) => (cb:Combo)=> {
-    const dcb = describe_combo(cb);
-    const lit = cb;
-    return {
-        0 : `${offset.toFixed().padStart(2)} (0-${cb}) : adv ${dcb} :  A := A / (2**${dcb})`,
-        1 : `${offset.toFixed().padStart(2)} (1-${cb}) : bxl ${lit} :  B := B ^ ${lit}`,
-        2 : `${offset.toFixed().padStart(2)} (2-${cb}) : bst ${dcb} :  B := ${dcb} & 7`,
-        3 : `${offset.toFixed().padStart(2)} (3-${cb}) : jnz ${lit} :  if (A==0) goto ${lit}`,
-        4 : `${offset.toFixed().padStart(2)} (4-${cb}) : bxc ${"_"} :  B := B ^ C`,
-        5 : `${offset.toFixed().padStart(2)} (5-${cb}) : out ${dcb} :  print(${dcb})`,
-        6 : `${offset.toFixed().padStart(2)} (6-${cb}) : bdv ${dcb} :  B := A / (2**${dcb})`,
-        7 : `${offset.toFixed().padStart(2)} (7-${cb}) : cdv ${dcb} :  C := A / (2**${dcb})`,
-    }[opcode]
-};
-
-const step = (stdout:(out:number)=>void)=>(cp:Computer)=>{
-    if (cp.pointer >= cp.program.length) throw new Error("eh");
-    const opcode = cp.program[cp.pointer];
-    const combo = cp.program[cp.pointer+1];
-    const instruction = decode(opcode);
-    //console.log(`>>> ${opcode}:${instruction.name} ${combo}`);
-    return instruction(stdout)(cp)(combo as any)
-}
 const quickstep = (stdout:(out:number)=>void)=>(cp:Computer)=>{
     if (cp.pointer >= cp.program.length) throw new Error("eh");
     // const opcode = cp.program[cp.pointer];
@@ -137,28 +13,8 @@ const quickstep = (stdout:(out:number)=>void)=>(cp:Computer)=>{
     const A = Math.floor(cp.A/8);
     return {...cp, A}
 }
-const print_state = (cp:Computer)=>{
-    console.log(`>>> ABC:  ${cp.A} ${cp.B} ${cp.C}  `)
-    console.log(`>>> PROG: ${cp.program}`);
-    console.log(`>>> P: ${
-            cp.pointer.toFixed(0).padStart(2)
-        } ${
-            Array.from({length:cp.pointer*2}).fill("-").join("")
-        }^ ^`
-    )
-    console.log(describe(cp.pointer)(cp.program[cp.pointer])(cp.program[cp.pointer+1]))
 
-}
-const run = (stdout:(out:number)=>void)=>(cp:Computer)=>{
-    let state = cp;
-    //print_state(state)
-    while(state.pointer<state.program.length){
-        //console.log("---")
-        state = step(stdout)(state)
-        //print_state(state)
-    }
-    return state
-}
+
 const search = (initial_computer:Computer)=>{
     for(let A=0; A<4e9; A++){
         let state = {...initial_computer, A};
@@ -215,6 +71,10 @@ function quick_run(A:number){
     }
     return out
 }
+function quick_step(A:number){
+    let b = (A & 7) ^ 1;
+    return ((b ^ Math.floor(A / (2**b))) ^ 6) & 7;
+}
 function quick_search(){
     const target = [2,4,1,1,7,5,4,0,0,3,1,6,5,5,3,0]
     console.log("Quick")
@@ -236,79 +96,42 @@ function quick_search(){
     console.log("Search")
 }
 
-function solve(input:string){
-    const computer = parse_input(input);
-    console.log("initial",  computer)
-    const stdout:number[] = []
-    const final_state = run(i=>stdout.push(i))(computer)
-    console.log("Output: ", stdout.join(","))
-    //console.log("Output: ", quick_run(computer.A).join(","))
-}
-// solve(`
-// Register A: 729
-// Register B: 0
-// Register C: 0
 
-// Program: 0,1,5,4,3,0
-// `)
-//solve(await Deno.readTextFile("input.txt"))
 
-function solve2(input:string){
-    
-    const computer = parse_input(input);
-    console.log("---")
-    console.log("Solve 2 - Initial Computer:")
-    print_state(computer)
-    console.log("---")
-    for(let offset =0;offset<computer.program.length;offset+=2){
-        console.log(describe(offset)(computer.program[offset])(computer.program[offset+1]))
-    }
-    console.log("---")
-    const self_replicator = search(computer)
-    console.log("Found!")
-    console.log("Value of A: "+self_replicator.A);
-}
-// solve2(`
+const input = await Deno.readTextFile("input.txt");
+// const input = `
 // Register A: 2024
 // Register B: 0
 // Register C: 0
 
 // Program: 0,3,5,4,3,0
-// `);
-//solve2(await Deno.readTextFile("input.txt"));
-
-// `
-// B1 = A1 & 7
-// B2 = B1 ^ 1
-// C1 = A1 / (2**B2)
-// B3 = B2 ^ C1
-// A2 = A1 / 8
-// B4 = B3 ^ 6
-// pritn(B4)
-// jnz A2
+// `;
+const computer = parse_input(input);
+print_state(computer);
+console.log(decompile(computer));
+console.log("Output", run_to_list(computer).join(","));
+console.log("Output Quick", quick_run(computer.A).join(","));
 
 
+console.log("...")
+let n = (((56*8+2)*8+6)*8+4)*8+2
+console.log(quick_run(n))
+console.log(quick_step(n))
+console.log("...")
 
-// B2 = (A1 & 7) ^ 1
-// C1 = A1 / (2**((A1 & 7) ^ 1))
-// B3 = (((A1 & 7) ^ 1) ^ (A1 / (2**((A1 & 7) ^ 1))))
-// A2 = A1 / 8
-// B4 = (((A1 & 7) ^ 1) ^ (A1 / (2**((A1 & 7) ^ 1)))) ^ 6
-// pritn(B4)
-// jnz A2
-//`
+let target = computer.program.reverse()
+let tp=0;
+let stages:number[] = []
+let pin = 0;
+while(stages.length<target.length){
+    const expected_result = target[tp]
+    const base = 0;
+    for(let stage of stages){
+        base = base*8+stage;
+    }
+    for(let pin=0;pin<8;pin++){
+        quick_step()
+    }
+}
 
-let input = parse_input(await Deno.readTextFile("input.txt"));
-//solve(await Deno.readTextFile("input.txt"))
-//console.log(quick_search())
-console.log("target",input.program)
-run(console.log)({...input,A:3933903358*8*8})
-// quick: 1,6,3,6,5,6,5,1,7
-//        1,6,3,6,5,6,5,1,7
-
-// pointer 11 A 3933903034
-// pointer 11 A 3933903035
-// pointer 11 A 3933903039
-// pointer 11 A 3933903354
-// pointer 11 A 3933903355
-// pointer 11 A 3933903358
+//console.log(run_until_mismatch(computer.program)({...computer, A:3933903034}))
---
+++ 17 - Computer/util/computer.ts
@@ -0,0 +1,175 @@
+export type Computer = {
+  A: number;
+  B: number;
+  C: number;
+  program: number[];
+  pointer: number;
+};
+type Instruction = (stdout: (out: number) => void) => (c: Computer) => (
+  ((cb: Combo) => Computer) | ((cb: Literal) => Computer));
+type Combo = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+type Literal = number;
+
+const combo = (c: Computer) => (combo: Combo) => {
+  switch (combo) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      return combo;
+    case 4:
+      return c.A;
+    case 5:
+      return c.B;
+    case 6:
+      return c.C;
+    default:
+      throw new Error("Invalid combo");
+  }
+};
+
+const adv: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (cb: Combo) => ({
+  ...cp,
+  pointer: cp.pointer + 2,
+  A: Math.floor(
+    cp.A / Math.pow(2, combo(cp)(cb))
+  )
+});
+const bxl: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (lit: number) => ({
+  ...cp,
+  pointer: cp.pointer + 2,
+  B: cp.B ^ lit
+});
+const bst: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (cb: Combo) => ({
+  ...cp,
+  pointer: cp.pointer + 2,
+  B: combo(cp)(cb) & 7
+});
+const jnz: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (lit: number) => ({
+  ...cp,
+  pointer: cp.A === 0 ? cp.pointer + 2 : cp.pointer = lit,
+});
+const bxc: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (_: number) => ({
+  ...cp,
+  pointer: cp.pointer + 2,
+  B: cp.B ^ cp.C
+});
+const out: Instruction = (stdout: (out: number) => void) => (cp: Computer) => (cb: Combo) => {
+  stdout(combo(cp)(cb) % 8);
+  return {
+    ...cp,
+    pointer: cp.pointer + 2,
+  };
+};
+const bdv: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (cb: Combo) => ({
+  ...cp,
+  pointer: cp.pointer + 2,
+  B: Math.floor(
+    cp.A / Math.pow(2, combo(cp)(cb))
+  )
+});
+const cdv: Instruction = (_stdout: (out: number) => void) => (cp: Computer) => (cb: Combo) => ({
+  ...cp,
+  pointer: cp.pointer + 2,
+  C: Math.floor(
+    cp.A / Math.pow(2, combo(cp)(cb))
+  )
+});
+
+export const decode = (opcode: number) => [
+  adv, bxl, bst, jnz, bxc, out, bdv, cdv
+][opcode];
+
+
+export const step = (stdout:(out:number)=>void)=>(cp:Computer)=>{
+    if (cp.pointer >= cp.program.length) throw new Error("eh");
+    const opcode = cp.program[cp.pointer];
+    const combo = cp.program[cp.pointer+1];
+    const instruction = decode(opcode);
+    return instruction(stdout)(cp)(combo as any)
+}
+
+export const run = (stdout:(out:number)=>void)=>(cp:Computer)=>{
+    let state = cp;
+    //print_state(state)
+    while(state.pointer<state.program.length){
+        //console.log("---")
+        state = step(stdout)(state)
+        //print_state(state)
+    }
+    return state
+}
+export const run_generate = function* (cp:Computer){
+    let state = cp;
+    while(state.pointer<state.program.length){
+        let out = undefined;
+        state = step(o=>out=o)(state);
+        if(out!==undefined){
+            yield out
+        }
+    }
+}
+
+export const run_to_console = run(i=>console.log(i))
+export const run_to_list = (cp:Computer)=>{
+    const result:number[] = [];
+    run(i=>result.push(i))(cp);
+    return result;
+}
+export const run_until_mismatch = (expected_output:number[]) => (cp:Computer) =>{
+    let pointer = 0;
+    for(const out of run_generate(cp)){
+        if(pointer>=expected_output.length){
+            return {program_finished:false, pointer, all_expected:false}
+        }
+        const expected = expected_output[pointer]
+        if(expected!==out){
+            return {program_finished:false, pointer, all_expected:false}
+        }
+        pointer++;
+    }
+    return {complete:true, pointer, all_expected:pointer==expected_output.length}
+}
+
+const describe_combo = (cb:Combo)=>({
+    0:"0",
+    1:"1",
+    2:"2",
+    3:"3",
+    4:"A",
+    5:"B",
+    6:"C",
+}[cb])
+const describe = (offset:number) => (opcode:number) => (cb:Combo)=> {
+const dcb = describe_combo(cb);
+const lit = cb;
+return {
+    0 : `${offset.toFixed().padStart(2)} (0-${cb}) : adv ${dcb} ::    A := A / (2**${dcb})`,
+    1 : `${offset.toFixed().padStart(2)} (1-${cb}) : bxl ${lit} ::    B := B ^ ${lit}`,
+    2 : `${offset.toFixed().padStart(2)} (2-${cb}) : bst ${dcb} ::    B := ${dcb} & 7`,
+    3 : `${offset.toFixed().padStart(2)} (3-${cb}) : jnz ${lit} ::    if (A==0) goto ${lit}`,
+    4 : `${offset.toFixed().padStart(2)} (4-${cb}) : bxc ${"_"} ::    B := B ^ C`,
+    5 : `${offset.toFixed().padStart(2)} (5-${cb}) : out ${dcb} ::    print(${dcb})`,
+    6 : `${offset.toFixed().padStart(2)} (6-${cb}) : bdv ${dcb} ::    B := A / (2**${dcb})`,
+    7 : `${offset.toFixed().padStart(2)} (7-${cb}) : cdv ${dcb} ::    C := A / (2**${dcb})`,
+}[opcode]
+};
+export const print_state = (cp:Computer)=>{
+    console.log(`>>> ABC:  ${cp.A} ${cp.B} ${cp.C}  `)
+    console.log(`>>> PROG: ${cp.program}`);
+    console.log(`>>> P: ${
+            cp.pointer.toFixed(0).padStart(2)
+        } ${
+            Array.from({length:cp.pointer*2}).fill("-").join("")
+        }^ ^`
+    )
+}
+export const decompile = (cp:Computer)=>{
+    const out:string[] = ["---"]
+    for(let pointer = 0;pointer<cp.program.length;pointer+=2){
+        out.push(
+            describe(cp.pointer)(cp.program[pointer])(cp.program[pointer+1] as Combo)!
+        )
+    }
+    return [...out,"---"].join("\n")
+}
